9fffcfd0c59b217279aaf9ac854dd713ff84cbe5
ViniciusItaloDaCruz <viniciusidacruz@hotmail.com>
2022-03-04 19:13:30 -0300

    Inicializando tela de cadastro

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text } from 'react-native';
 
 import { HeaderComponent } from '../../components/Header';
+import { InputFieldComponent } from '../../components/InputField';
 
 import { AppProps } from '../../routes/types/app';
 
 export function RegisterScreen({ navigation }: AppProps) {
+  const [name, setName] = useState('')
 
   return (
     <>
       <HeaderComponent>
         <Text>Register</Text>
       </HeaderComponent>
+
+      <InputFieldComponent
+              primary
+              value={name}
+              placeholder="*Nome"
+              onChangeText={(value) => setName(value)}
+            />
     </>
   );
 }
