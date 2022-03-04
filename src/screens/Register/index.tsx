a7d029b6e706623d341aced22006096e56850ae1
ViniciusItalo <viniciusidacruz@hotmail.com>
2022-03-04 19:51:30 -0300

    Estrutura inicial da tela de cadastro

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -5,9 +5,18 @@ import { HeaderComponent } from '../../components/Header';
 import { InputFieldComponent } from '../../components/InputField';
 
 import { AppProps } from '../../routes/types/app';
+import FormFields from '../../common/constants/form';
+
+import * as S from './styles'
 
 export function RegisterScreen({ navigation }: AppProps) {
-  const [name, setName] = useState('')
+  const [cep, setCep] = useState('');
+  const [city, setCity] = useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [district, setDistrict] = useState('');
 
   return (
     <>
@@ -15,12 +24,62 @@ export function RegisterScreen({ navigation }: AppProps) {
         <Text>Register</Text>
       </HeaderComponent>
 
-      <InputFieldComponent
+      <S.Form>
+        <InputFieldComponent
+          primary
+          value={name}
+          placeholder={`* ${FormFields.FULL_NAME}`}
+          onChangeText={(value) => setName(value)}
+        />
+
+        <InputFieldComponent
+          primary
+          value={phone}
+          placeholder={`* ${FormFields.PHONE}`}
+          onChangeText={(value) => setPhone(value)}
+        />
+
+        <InputFieldComponent
+          primary
+          value={email}
+          placeholder={`* ${FormFields.EMAIL}`}
+          onChangeText={(value) => setEmail(value)}
+        />
+
+        <InputFieldComponent
+          primary
+          value={address}
+          placeholder={`* ${FormFields.ADDRESS}`}
+          onChangeText={(value) => setAddress(value)}
+        />
+
+        <S.GridForm>
+          <S.GridItem>
+            <InputFieldComponent
               primary
-              value={name}
-              placeholder="*Nome"
-              onChangeText={(value) => setName(value)}
+              value={district}
+              placeholder={`* ${FormFields.DISTRICT}`}
+              onChangeText={(value) => setDistrict(value)}
             />
+          </S.GridItem> 
+
+          <S.GridItem>
+            <InputFieldComponent
+              primary
+              value={cep}
+              placeholder={`* ${FormFields.CEP}`}
+              onChangeText={(value) => setCep(value)}
+            />
+          </S.GridItem>
+        </S.GridForm>
+
+        <InputFieldComponent
+          primary
+          value={city}
+          placeholder={`* ${FormFields.CITY}`}
+          onChangeText={(value) => setCity(value)}
+        />
+      </S.Form>
     </>
   );
 }
---
+++ src/screens/Register/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components/native";
+
+export const Form = styled.View`
+    padding-left: 15;
+    padding-right: 15;
+
+    margin-top: 15;
+`;
+
+export const GridForm = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const GridItem = styled.View`
+    width: 48%;
+`
